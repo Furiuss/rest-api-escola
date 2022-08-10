4f133f2482cce4e1d2395d1e376b72ff6b7251b2
André <andremessias.az@gmail.com>
2022-08-10 19:13:01 -0300

    add: install prisma, maybe will be used later

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ class App {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
+    this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
   }
 
   routes() {
